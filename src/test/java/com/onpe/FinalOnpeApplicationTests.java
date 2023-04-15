bbc176081a18127e20e954cebcee3e728e653ac0
Your Name <you@example.com>
2023-04-15 00:37:26 -0500

    creando el proyecto final

---
+++ src/test/java/com/onpe/FinalOnpeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onpe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalOnpeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
