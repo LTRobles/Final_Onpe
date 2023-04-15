25d8bbc5eefe08124c260a73b8eba1514e20b0bd
Your Name <you@example.com>
2023-04-15 01:23:08 -0500

    actua

--- src/main/java/web/onpe/repository/IGrupoVotacion.java
+++ src/main/java/web/onpe/repository/IGrupoVotacion.java
@@ -1,5 +1,14 @@
 package web.onpe.repository;
 
-public interface IGrupoVotacion {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import web.onpe.model.GrupoVotacion;
+
+@Repository
+public interface IGrupoVotacion extends JpaRepository<GrupoVotacion ,String> {
+	@Query(value = "{call usp_getGrupoVotacion(:id)}", nativeQuery = true)
+	GrupoVotacion getGrupoVotacion(String id);
 
 }
